a5be1e60f86d528198f8c6df944e2a3c0cf0ad3b
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-12 15:25:14 +0530

    Creating the main file app.js

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+})
+
+module.exports = app;
