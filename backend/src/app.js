719fbed96ecc8a333270f1802bd67640df586863
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-18 14:24:40 +0530

    update app.js file to improve backend functionality

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require("express");
+const aiRoutes = require("./routes/ai.routes")
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+})
+
+app.use('/ai', aiRoutes)
+
+module.exports = app;
