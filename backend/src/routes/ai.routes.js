e5f12fb3ba57802ad460afb139a0ce2776d6e296
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-19 14:12:01 +0530

    update ai.routes.js to improve AI-Related routes

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const aiController = require('../controllers/ai.controller')
 const router = express.Router();
 
-router.get('/get-response', 
-    aiController.getResponse
+router.post('/get-review', 
+    aiController.getReview
 )
 
 module.exports = router
